c8a8f2d1dda705b6a6fc4b9fbc58c4e0334ea3cc
千田大貴 <hiroki@appurunoMacBook-puro.local>
2018-09-08 15:14:29 +0900

    画像アップロード処理修正

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jquery-ui-rails'
 gem 'jquery-rails'
 gem "paperclip", "~> 6.0.0"
 gem 'jquery-turbolinks'
+gem 'dropzonejs-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180907155834) do
+ActiveRecord::Schema.define(version: 20180908055515) do
 
   create_table "listings", force: :cascade do |t|
     t.string "home_type"
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 20180907155834) do
     t.integer "listing_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo_file_name"
-    t.string "photo_content_type"
-    t.integer "photo_file_size"
-    t.datetime "photo_updated_at"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["listing_id"], name: "index_photos_on_listing_id"
   end
 
